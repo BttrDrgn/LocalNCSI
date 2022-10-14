c8248829c7187f0b46af4f0ecc62a60d3a50dee0
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-10-14 14:43:36 -0500

    Removed test counting code

--- src/main.cpp
+++ src/main.cpp
@@ -3,9 +3,8 @@ int main(int argc, char* argv[])
 #ifdef WIN32
 	SetConsoleTitleA(APPNAME);
 #endif
-	
+
 	httplib::Server server;
-	static int count = 0;
 
 	server.Get("/", [](const auto& req, auto& res)
 	{
@@ -17,14 +16,12 @@ int main(int argc, char* argv[])
 	{
 		res.set_content("Microsoft NCSI", "text/plain");
 		res.status = 200;
-		++count;
 	});
 
 	server.Get("/connecttest.txt", [](const auto& req, auto& res)
 	{
 		res.set_content("Microsoft Connect Test", "text/plain");
 		res.status = 200;
-		++count;
 	});
 
 	server.listen("localhost", 80);
